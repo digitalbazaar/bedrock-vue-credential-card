ec5bb8adb215a7606f999c0317b3360ebc1d2e69
Dave Longley <dlongley@digitalbazaar.com>
2018-09-23 19:18:18 -0400

    Fix test config and add postinstall script.

--- test/test.config.js
+++ test/test.config.js
@@ -8,3 +8,11 @@ const path = require('path');
 
 config.karma.suites['bedrock-vue-credential-card'] =
   path.join('web', '**', '*.js');
+
+// TODO: consider moving to `bedrock-karma`?
+config.karma.config.webpack.resolve = {
+  alias: {
+    "quasar-framework$": path.resolve(
+      __dirname, 'node_modules/quasar-framework/dist/umd/quasar.mat.umd.js')
+  }
+};
