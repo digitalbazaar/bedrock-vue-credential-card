d125d0f86087fbd7d249d82ea4c50edf7eb0b09c
Dave Longley <dlongley@digitalbazaar.com>
2018-09-25 16:02:12 -0400

    Update "show mode" to include field name.

--- CredentialCardField.vue
+++ CredentialCardField.vue
@@ -6,28 +6,37 @@
       'background-color': (visibilityToggle && visible) ?
         visibleBackgroundColor : 'inherit',
       cursor: visibilityToggle ? 'pointer' : 'inherit',
-      'font-weight': (!visibilityToggle || visible) ? 'bold' : 'normal',
       'margin-bottom': visibilityToggle ? '1px' : '0',
-      padding: visibilityToggle ? '0 16px' : '8px 16px'
+      padding: visibilityToggle ? '4px 16px' : '8px 16px'
     }">
-    <q-item-side v-if="visibilityToggle">
-      <q-icon
-        style="font-size: 20px"
-        :name="visible ? hiddenIcon : visibleIcon"
-        :color="visible ? 'primary' : 'faded'" />
-    </q-item-side>
     <q-item-main v-if="visibilityToggle">
       <q-item-tile
         label
         lines="1"
-        :text-color="visible ? 'primary' : 'dark'">
-        {{visible ? value : name}}
+        v-bind:style="{
+          'font-weight': visible ? 'bold' : 'inherit',
+          color: visible ? darkerPrimary : 'black'
+        }">
+        {{name}}
+      </q-item-tile>
+      <q-item-tile
+        sublabel
+        lines="1"
+        :text-color="visible ? 'primary' : 'faded'">
+        <span v-if="visible">{{value}}</span>
+        <span v-else>&bull;&bull;&bull;&bull;</span>
       </q-item-tile>
     </q-item-main>
     <q-item-main v-else>
       <q-item-tile label lines="1">{{name}}</q-item-tile>
       <q-item-tile sublabel lines="1">{{value}}</q-item-tile>
     </q-item-main>
+    <q-item-side v-if="visibilityToggle" right>
+      <q-icon
+        style="font-size: 20px"
+        :name="visible ? hideIcon : showIcon"
+        :color="visible ? 'primary' : 'faded'" />
+    </q-item-side>
   </q-item>
 </template>
 <script>
@@ -40,12 +49,12 @@ import Quasar from 'quasar-framework';
 
 const DEFAULT_ICONS = {
   fontawesome: {
-    visible: 'fas fa-eye',
-    hidden: 'fas fa-eye-slash'
+    showField: 'fas fa-eye',
+    hideField: 'fas fa-eye-slash'
   },
   'material-icons': {
-    visible: 'visibility',
-    hidden: 'visibility_off'
+    showField: 'visibility',
+    hideField: 'visibility_off'
   }
 };
 
@@ -59,19 +68,18 @@ export default {
       this.$q.icon.credentialCardField = {};
     }
     const {credentialCardField: icons} = this.$q.icon;
-    if(!icons.visible) {
-      icons.visible = defaultIcons.visible;
-    }
-    if(!icons.hidden) {
-      icons.hidden = defaultIcons.hidden;
+    for(const name in defaultIcons) {
+      if(!icons[name]) {
+        icons[name] = defaultIcons[name];
+      }
     }
   },
   computed: {
-    visibleIcon() {
-      return this.$q.icon.credentialCardField.visible;
+    showIcon() {
+      return this.$q.icon.credentialCardField.showField;
     },
-    hiddenIcon() {
-      return this.$q.icon.credentialCardField.hidden;
+    hideIcon() {
+      return this.$q.icon.credentialCardField.hideField;
     },
     visibleBackgroundColor() {
       const {utils: {colors}} = Quasar;
@@ -79,6 +87,12 @@ export default {
       const rgba = colors.hexToRgb(primary);
       rgba.a = 0.15;
       return `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`;
+    },
+    darkerPrimary() {
+      const {utils: {colors}} = Quasar;
+      const primary = colors.getBrand('primary');
+      const darker = colors.lighten(primary, -25);
+      return darker;
     }
   },
   props: {
