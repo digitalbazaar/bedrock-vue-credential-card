7e61037d4252d77052f9eb8ba8d6273bb3f09649
Ganesh Annan <gannan@digitalbazaar.com>
2019-02-20 17:43:20 -0500

    Add the ability to reference images dir with /images.

--- test/config.js
+++ test/config.js
@@ -9,6 +9,11 @@ require('bedrock-views');
 
 config.paths.cache = path.join(__dirname, '.cache');
 
+// add images
+config.express.static.push({
+  route: '/images',
+  path: path.join(__dirname, 'components', 'images')
+});
 // add pseudo packages
 config.views.system.packages.push({
   path: path.join(__dirname, 'components'),
