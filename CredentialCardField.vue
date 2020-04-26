1724870bcbd4c94c747801f63de2fdf143873958
Manu Sporny <msporny@digitalbazaar.com>
2020-04-26 16:02:57 -0400

    Add support for wide images.

--- CredentialCardField.vue
+++ CredentialCardField.vue
@@ -69,6 +69,12 @@
           :src="value" />
       </q-item-section>
       <q-item-section
+        v-else-if="component === 'WideImage'"
+        class="g-field-data-regular">
+        <q-img style="max-width: 100%; min-width: 100%;"
+          class="rounded-borders" :src="value" />
+      </q-item-section>
+      <q-item-section
         v-else-if="component === 'FixedWidth'"
         class="g-field-data-regular">
         <pre
