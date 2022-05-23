6196e7f0965f1e8121a99dc9fb1e717712883d02
Dave Longley <dlongley@digitalbazaar.com>
2022-05-23 12:45:40 -0400

    Fix karma test config.

--- test/test.config.js
+++ test/test.config.js
@@ -2,19 +2,12 @@
  * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
-import {fileURLToPath} from 'url';
 import path from 'path';
 import '@bedrock/karma';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 config.karma.suites['bedrock-vue-credential-card'] =
   path.join('web', '**', '*.js');
-
-// TODO: consider moving to `bedrock-karma`?
-config.karma.config.webpack.resolve = {
-  alias: {
-    quasar$: path.resolve(
-      __dirname, 'node_modules/quasar/dist/quasar.umd.js')
-  }
+config.karma.config.proxies = {
+  '/': 'https://localhost:18443'
 };
+config.karma.config.proxyValidateSSL = false;
