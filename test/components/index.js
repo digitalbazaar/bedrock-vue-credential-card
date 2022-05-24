8a736f8f08f329bd3097eb1b5a85a3c7421af456
Dave Longley <dlongley@digitalbazaar.com>
2022-05-24 11:11:49 -0400

    Apply Vue3 updates.

--- test/components/index.js
+++ test/components/index.js
@@ -1,47 +1,45 @@
 /*!
  * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
  */
-// import * as brQuasar from '@bedrock/quasar';
-// import {config} from '@bedrock/web';
+import * as brQuasar from '@bedrock/quasar';
 import * as brVue from '@bedrock/vue';
+// import {config} from '@bedrock/web';
+import {createRouter, createWebHistory} from 'vue-router';
 import {default as iconSet} from 'quasar/icon-set/fontawesome-v5.js';
 import Quasar from 'quasar';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import TestApp from '../components/TestApp.vue';
 
 import './app.less';
 import '@bedrock/web-fontawesome';
 
-// FIXME: make configurable
-Vue.config.devtools = true;
+brVue.initialize({
+  async beforeMount({app}) {
+    // show devtools
+    app.config.devtools = true;
 
-// install all plugins
-Vue.use(brVue);
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: [{
+        path: '/',
+        component: () => import('./TestHome.vue'),
+        meta: {title: 'Credential Card'}
+      }]
+    });
+    brVue.augmentRouter({app, router});
+    app.use(router);
 
-brVue.setRootVue(async () => {
-  // replace default `br-root` with a custom one
-  // eslint-disable-next-line vue/component-definition-name-casing
-  Vue.component('br-root', () => import('./BrRoot.vue'));
+    // initialize br-quasar
+    await brQuasar.initialize({app});
 
-  const router = new VueRouter({
-    mode: 'history',
-    routes: [{
-      path: '/',
-      component: () => import('./TestHome.vue'),
-      meta: {title: 'Credential Card'}
-    }]
-  });
+    // configure quasar
+    //iconSet.credentialCard = {defaultImage: 'fas fa-check'};
+    Quasar.iconSet.set(iconSet);
+    // await brQuasar.theme({
+    //   brand: {
+    //     primary: '#xxxxxx'
+    //   }
+    // });
 
-  // configure quasar
-  //iconSet.credentialCard = {defaultImage: 'fas fa-check'};
-  Quasar.iconSet.set(iconSet);
-  // await brQuasar.theme({
-  //   Quasar,
-  //   brand: {
-  //     primary: '#xxxxxx'
-  //   }
-  // });
-
-  const BrApp = Vue.component('br-app');
-  return new BrApp({router});
+    return TestApp;
+  }
 });
