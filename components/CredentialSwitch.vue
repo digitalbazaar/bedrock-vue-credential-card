3d2a6e2dceb2104432ab3692847c84b3e91c6f8d
Tyler Olson <tolson@dlinc.io>
2022-06-21 17:06:41 -0400

    Initial framework to register display components for credentials.

---
+++ components/CredentialSwitch.vue
@@ -0,0 +1,31 @@
+<template>
+  <component
+    :is="selection"
+    :v-bind="$attrs" />
+</template>
+
+<script setup>
+import {computed, defineProps} from 'vue';
+
+const componentDefaults = {
+  list: 'CredentialBase',
+  details: 'CredentialDetail'
+};
+
+const props = defineProps({
+  mode: {
+    type: String,
+    default: 'list'
+  }
+});
+
+const selection = computed(() => {
+  const {mode} = props;
+  return componentDefaults[mode];
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
