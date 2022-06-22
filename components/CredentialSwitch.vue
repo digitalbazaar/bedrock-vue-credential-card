666c8fb87f80b1257ed75538d36e25801587d652
Tyler Olson <tolson@dlinc.io>
2022-06-22 16:10:08 -0400

    Implement rudimentary config system for credential display components.

--- components/CredentialSwitch.vue
+++ components/CredentialSwitch.vue
@@ -1,27 +1,45 @@
 <template>
   <component
     :is="selection"
-    :v-bind="$attrs" />
+    v-bind="{...$props, ...$attrs}" />
 </template>
 
 <script setup>
-import {computed, defineProps} from 'vue';
+import {computed, defineProps, unref} from 'vue';
+import {config} from '@bedrock/web';
+import CredentialBase from './CredentialBase.vue'
+import CredentialDetail from './CredentialDetail.vue'
 
 const componentDefaults = {
-  list: 'CredentialBase',
-  details: 'CredentialDetail'
+  list: CredentialBase,
+  details: CredentialDetail
 };
 
 const props = defineProps({
   mode: {
     type: String,
     default: 'list'
+  },
+  credential: {
+    type: Object,
+    required: true
   }
 });
 
 const selection = computed(() => {
-  const {mode} = props;
-  return componentDefaults[mode];
+  const {credentialDisplay: {registration: componentRegistration}} = config;
+  const {mode, credential} = props;
+  const {type} = unref(credential);
+  const registrations = componentRegistration.filter(r => r.mode === mode);
+  const granularType = type[type.length - 1];
+  const options = registrations.map(r => {
+    if(r.acceptableTypes.includes(granularType)) {
+      return `${r.prefix ?? ''}${r.component}`;
+    }
+  });
+  console.log(options)
+  options.push(componentDefaults[mode]);
+  return options[0];
 });
 
 </script>
